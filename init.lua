2bc5ca31573682d69bbe07c075f83e40a76bfb8c
Ilia Levikov <ilia.levikov@tempus.com>
2024-03-13 11:08:34 +1300

    remove indent lines

--- init.lua
+++ init.lua
@@ -728,7 +728,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   require 'kickstart.plugins.debug',
-  require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.indent_line',
   --
   -- NOTE: Harpoon setup
   {
