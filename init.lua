a1a269a8ce60913e58a322ea2f940601edb5be91
Ilian <fonych1@gmail.com>
2024-03-13 23:01:54 +1300

    add remaps

--- init.lua
+++ init.lua
@@ -65,13 +65,20 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Set terminal gui colors
+vim.opt.termguicolors = true
+
+-- Add key to directory tree
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Diagnostic keymaps
@@ -79,6 +86,22 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Visual movements keymaps
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+--paste over visual
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- Ctr-C == ESC in terms of Ctr-v
+vim.keymap.set('i', '<C-c>', '<Esc>')
+
+-- Replace a word
+vim.keymap.set('n', '<leader>sp', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
