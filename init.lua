6347b701ead9247902615f8007b6bf3b1ac18a0f
Ilian <fonych1@gmail.com>
2024-03-15 00:00:51 +1300

    add remap for moving block of V text

--- init.lua
+++ init.lua
@@ -81,6 +81,10 @@ vim.opt.termguicolors = true
 -- Add key to directory tree
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
+-- Move V text up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv'")
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv'")
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
